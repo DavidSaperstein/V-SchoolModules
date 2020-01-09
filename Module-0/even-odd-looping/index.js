6976761a85966de3e8f08cb2588afff2bb7c7afe
David <d.h.saperstein@gmail.com>
2020-01-09 09:40:30 -0700

    even-odd-looping complete

---
+++ Module-0/even-odd-looping/index.js
@@ -0,0 +1,15 @@
+// let num = 0
+
+for (var i = 0; i <= 10; i++) {
+
+    i%2 === 0 ? console.log("even") : console.log ("odd")
+}
+//     if (i%2 === 0)
+//         {
+//             console.log("even")
+            
+//     } else {
+//         console.log("odd")
+        
+//     }
+// 
