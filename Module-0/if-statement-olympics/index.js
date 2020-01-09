ed124d504fedcf69b36fa29c78d2c49d0866fb06
David <d.h.saperstein@gmail.com>
2020-01-08 17:21:59 -0700

    bronze medal

---
+++ Module-0/if-statement-olympics/index.js
@@ -0,0 +1,31 @@
+// if (5 > 3) {
+//     console.log("is greater than")
+// }
+
+// if (cat.length === 3 ) {
+//     return "is the lengh"
+// }
+
+// if ("cat" !== "dog") { 
+//     return "not the same"        
+// }
+
+
+var person = {
+    name: "Bobby",
+    age: 12
+}
+// console.log(person)
+
+// if (person.age >= 18) {
+//     console.log(person.name + " is allowed to go to the movie")
+// }
+
+// if (person.name[0] === "B") {
+//     console.log(person.name + " is allowed to go to the movie")
+// }
+
+// if (person.name[0] === "B" && person.age > 18) {
+//     console.log(person.name + " is allowed to go to the movie")
+// }
+
