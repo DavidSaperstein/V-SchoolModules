40ab16183abf4d179f20b17917665be888ab02a0
David <d.h.saperstein@gmail.com>
2020-01-10 17:02:12 -0700

    finished exercise-functions except last optional question

--- Module-0/exercise-functions/indext.js
+++ Module-0/exercise-functions/indext.js
@@ -28,11 +28,38 @@ function stringy(string) {
         var newString = string.concat(string);
         console.log(newString)
     } else {
-        var halfString = Math.ceil(string.substr(0,string.length / 2));
-        // var newString = string.substr(0, string / 2)
-        console.log(halfString)
+        if (string.lengh%2 === 0) {
+            var halfString = string.substring(0, string.length/2);
+        }
+        else {
+            var newString = string.substring(0, string.length/2+1);
+            console.log(newString)
+             }
 
     }
 }
 stringy("david")
 stringy("threethreethreethreethree")
+
+function fibonacci(n) {
+    let fibAdd = 0
+    let fibSum = 0
+    for (i = 0; i < n; i++) {
+        if (i === 0) {
+            var fibArray = [1];
+            fibSum += fibArray[i]; 
+        }  else if (fibArray.length === 1) {
+            fibArray.push(1);
+            fibSum += fibArray[i];
+
+        } else {
+                fibAdd = fibArray[i-1] + fibArray[i-2];
+                fibArray.push(fibAdd);
+                fibSum += fibArray[i];                
+        }
+    }
+    console.log(fibArray) + console.log(fibSum)           
+    }
+
+
+fibonacci(6)
