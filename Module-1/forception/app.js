ab8669048d2e9120142470e23139a6b5264a7344
David <d.h.saperstein@gmail.com>
2020-02-05 12:55:21 -0700

    forception complete

--- Module-1/forception/app.js
+++ Module-1/forception/app.js
@@ -1,8 +1,15 @@
 var people = ["Jon", "Jacob", "Jingle", "Heimer", "Schmidt"]
 var alphabet = "abcdefghijklmnopqrstuvwxyz"
 
-function forception(people, alphebet) {
+function forception(people, alphabet) {
+    const peoplphabet = []
     for (let i = 0; i < people.length; i++) {
-        
+        peoplphabet.push(people[i].concat(":"))
+        for (let j = 0; j < alphabet.length; j++) {
+            peoplphabet.push(alphabet[j].toUpperCase())
+        }
     }
+    return peoplphabet
 }
+
+console.log(forception(people, alphabet))
