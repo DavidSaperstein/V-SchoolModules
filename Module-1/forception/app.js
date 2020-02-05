2074eba5caa06132b70e5ea50a946d7ed5181cf3
David <d.h.saperstein@gmail.com>
2020-02-04 18:14:27 -0700

    started forception

---
+++ Module-1/forception/app.js
@@ -0,0 +1,8 @@
+var people = ["Jon", "Jacob", "Jingle", "Heimer", "Schmidt"]
+var alphabet = "abcdefghijklmnopqrstuvwxyz"
+
+function forception(people, alphebet) {
+    for (let i = 0; i < people.length; i++) {
+        
+    }
+}
