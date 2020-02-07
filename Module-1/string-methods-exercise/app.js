2fec21a1c6df147fc2d5d2ee35bc469bf84f99dd
DavidSaperstein <d.h.saperstein@gmail.com>
2020-02-06 22:54:32 -0700

    finished string methods exercise

--- Module-1/string-methods-exercise/app.js
+++ Module-1/string-methods-exercise/app.js
@@ -13,3 +13,24 @@ function numberHalf(number) {
 }
 
 console.log(numberHalf("567"))
+
+function capitalLowerCase(string) {
+    let firstHalf = string.slice(0, Math.floor(string.length/2)).toUpperCase()
+    let secondHalf = string.slice(Math.floor(string.length/2), string.length).toLowerCase()
+    return firstHalf+secondHalf
+}
+
+console.log(capitalLowerCase("The Mayor of Value Town"))
+
+
+function capitalSpace(string) {
+    let stringArr = string.split("")
+    for (let i = 0; i < stringArr.length; i++) {
+        if (stringArr[i-1] === " " || i === 0) {
+            stringArr[i] = stringArr[i].toUpperCase()
+        }
+    }
+    return stringArr.join("")
+}
+
+console.log(capitalSpace("the mayor of value town"))
