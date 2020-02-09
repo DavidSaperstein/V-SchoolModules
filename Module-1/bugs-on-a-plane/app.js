9e1f599b2ac3f1020bd4d05d7cb3fcb41b83bca7
David <d.h.saperstein@gmail.com>
2020-02-08 17:39:45 -0700

    bugs on a plane complete

--- Module-1/bugs-on-a-plane/app.js
+++ Module-1/bugs-on-a-plane/app.js
@@ -1,21 +1,19 @@
-var form = document.getElementByID("airline-form");
-var submit = document.getElementByID("submit");
-var query = document.querySelector;
+var form = document.getElementById("airline-form");
+var submit = document.getElementById("submit");
+// var query = document.querySelector;
 
-function formAlert{} {
-    var firstName = form.elements["firstName"].value;
-    var lastName = form.elements["lastName"].value;
+function formAlert() {
+    var firstName = form.elements["first-name"].value;
+    var lastName = form.elements["last-name"].value;
     var age = form.elements["age"].value;
     var gender = form.elements["gender"].value;
     var location = form.elements["travel-location"].value;
-    var diet = {};
+    var diet = []
     if (form.elements['vegan'].checked) {
-        var diet.pop(document.getElementById("vegan").value);
-    }
-    if (form.elements['gluten'].checked) {
+        diet.push(document.getElementById("vegan").value);
+    } else if (form.elements['gluten'].checked) {
         diet.push(document.getElementById('gluten').value);
-    }
-    if (form.elements['paleo'].checked) {
+    } else if (form.elements['paleo'].checked) {
         diet.push(document.getElementById('paleo').value);
     }
 
