9ee44015d7ae6d5e59540a3f4105e3f69390582b
DavidSaperstein <d.h.saperstein@gmail.com>
2020-05-24 14:02:28 -0600

    POST complete'

--- Module-2/axios-todo/app.js
+++ Module-2/axios-todo/app.js
@@ -11,13 +11,12 @@ const createH1 = (listItem, key) => {
 }
 
 const showList = (listArray) => {
-    console.log(listArray)
+    clearList()
     if (listArray.length > 0) {
         listArray.map(todo => {
-            console.log(todo)
             const container = document.createElement("div")
             container.setAttribute("class", "todo-card")
-            document.body.appendChild(container)
+            document.getElementById('list').appendChild(container)
             const title = createH1(todo, "title")
             if (todo.completed) {
                 title.setAttribute("class", "strikethrough")
@@ -32,14 +31,38 @@ const showList = (listArray) => {
                 container.appendChild(price)
             }
             if (todo.imgUrl) {
-                console.log("this is working")
                 const img = document.createElement("img")
                 img.src = todo.imgUrl
-                console.log(todo.imgUrl)
                 img.style.width = "250px"
                 container.appendChild(img)
             }
         })
+    }
+}
+
+const post = (newToDo) => {
+    axios.post('https://api.vschool.io/davidsaperstein/todo', newToDo)
+    .then(res => getTodo())
+    .catch(err => console.log(err))
+}
+
+const form = document.form
+form.addEventListener('submit', function (e) {
+    e.preventDefault()
+    const newToDo = {
+        title: form.title.value,
+        price: form.price.value,
+        description: form.description.value,
+        imgUrl: form.imgUrl.value
+    }
+    console.log(newToDo)
+    post(newToDo)
+})
+
+const clearList = () => {
+    const el = document.getElementById('list')
+    while(el.firstChild) {
+        el.removeChild(el.firstChild)
     }
 }
 
