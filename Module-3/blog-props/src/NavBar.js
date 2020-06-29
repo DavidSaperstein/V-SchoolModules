6ed3a409b4c168f4b8b9d33409627876a4e2d152
DavidSaperstein <d.h.saperstein@gmail.com>
2020-06-29 16:36:58 -0600

    navbar complete

--- Module-3/blog-props/src/NavBar.js
+++ Module-3/blog-props/src/NavBar.js
@@ -4,13 +4,13 @@ export default function NavBar() {
     return (
         <div className="navbar">
             <div className="left-navbar-container">
-                <a>Start Bootstrap</a>
+                <a className="navlink">Start Bootstrap</a>
             </div>
             <div className="right-navbar-container">
-                <a>Home</a>
-                <a>About</a>
-                <a>Sample Post</a>
-                <a>Contact</a>
+                <a className="navlink">Home</a>
+                <a className="navlink">About</a>
+                <a className="navlink">Sample Post</a>
+                <a className="navlink">Contact</a>
             </div>  
         </div>
     )
