bcaf3e3173f4382f19017642e791be983adbc8d9
DavidSaperstein <d.h.saperstein@gmail.com>
2020-07-08 16:53:02 -0600

    small time dj button

--- Module-3/dj-react/src/App.js
+++ Module-3/dj-react/src/App.js
@@ -8,15 +8,35 @@ export default class App extends React.Component {
         this.state = {
             colors: ['white', 'white', 'white', 'white']
         }
+        this.smallTimeDJ = this.smallTimeDJ.bind(this)
+        this.partyDJ = this.partyDJ.bind(this)
+    }
+
+    smallTimeDJ() {
+        if (this.state.colors[0] === 'white') {
+            this.setState({colors: ['black', 'black', 'black', 'black']})
+        } else if (this.state.colors[0] === 'black') {
+            this.setState({colors: ['white', 'white', 'white', 'white']})
+        }
+    }
+
+    partyDJ() {
+        this.setState(prevState => {
+                return {
+                    colors: ['purple', 'purple', prevState.colors[2], prevState.colors[3]]
+                }
+        })
     }
 
     render() {
+        console.log(this.state.colors)
         return (
             <div className='flex-container'>
                 <div className='square-container'>
-                    {this.state.colors.map(color => <Square color={color} />)}
+                    {this.state.colors.map((color, i) => <Square key={i+1} color={color} number={i+1}/>)}
                 </div>
-                <button onClick={this.handleClick}>Small Time DJ</button>
+                <button className='buttons' onClick={this.smallTimeDJ}>Small Time DJ</button>
+                <button className='buttons' onClick={this.partyDJ}>Party DJ</button>
             </div>
         )
     }
