47c6bb3d358af921c000a3eaa9a5410e1cc32447
DavidSaperstein <d.h.saperstein@gmail.com>
2020-07-13 11:30:19 -0600

    react dice setup complete. all relevant files and outlines created

---
+++ Module-3/react-dice/src/App.js
@@ -0,0 +1,19 @@
+import React from "react"
+import "./styles.css" 
+
+export default class App extends React.Component {
+    constructor () {
+        super()
+        this.state = {
+
+        }
+    }
+
+    render () {
+        return (
+            <div className='dice-container'>
+                this is an app
+            </div>
+        )
+    }
+}
---
+++ Module-3/react-dice/src/DiceBox.js
@@ -0,0 +1,19 @@
+import React from "react"
+import Die from './Die'
+
+export default class DiceBox extends React.Component {
+    constructor() {
+        super()
+        this.state {
+
+        }
+    }
+
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
