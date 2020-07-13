a610a8c706cf9bff12a3eb6bbfcba0dca0ec6624
DavidSaperstein <d.h.saperstein@gmail.com>
2020-07-13 14:16:58 -0600

    preliminary css and roll button without selectors complete

--- Module-3/react-dice/src/DiceBox.js
+++ Module-3/react-dice/src/DiceBox.js
@@ -4,15 +4,32 @@ import Die from './Die'
 export default class DiceBox extends React.Component {
     constructor() {
         super()
-        this.state {
-            nums: [1, 1, 1, 1, 1, 1]
+        this.state = {
+            nums: [1, 1, 1, 1, 1]
         }
     }
 
+    randomNumGen = () => {
+        return Math.floor(Math.random() * (6-1+1)) + 1
+    }
+
+    rollDice = () => {
+        this.setState(prevState => {
+            const newNums = prevState.nums.map(num => this.randomNumGen())
+            return {nums: newNums}
+        })
+    }
+
     render() {
+        console.log(this.state.nums)
         return (
             <div>
-
+                <div className='dice-container'>
+                    {this.state.nums.map((num, i) => <Die key={i} num={num}/>)}
+                </div>
+                <div className='button-container'>
+                    <button id='button' onClick={() => this.rollDice()}>Roll</button>
+                </div>
             </div>
         )
     }
--- Module-3/react-dice/src/Die.js
+++ Module-3/react-dice/src/Die.js
@@ -1,9 +1,10 @@
 import React from 'react'
 
 export default function Die(props) {
+    const {num} = props
     return (
-        <div>
-            
+        <div className='die'>
+            {num}
         </div>
     )
 }
