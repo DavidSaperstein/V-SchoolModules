e17202cfb12086ef0a471576225e50b354cd54b4
DavidSaperstein <d.h.saperstein@gmail.com>
2020-07-15 19:20:32 -0600

    app setup complete

---
+++ Module-3/name-badge/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import BadgeForm from './BadgeForm'
+import BadgeList from './BadgeList'
+
+export default function App() {
+    return (
+        <div>
+            Some Text
+            {/* <BadgeForm />
+            <BadgeList /> */}
+        </div>
+    )
+}
