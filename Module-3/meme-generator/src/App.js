acb5e85d55d41b56b7ec70e520422ab166846b02
DavidSaperstein <d.h.saperstein@gmail.com>
2020-07-29 00:27:14 -0600

    create Header and MemeGenerator components

--- Module-3/meme-generator/src/App.js
+++ Module-3/meme-generator/src/App.js
@@ -1,10 +1,13 @@
 import React from 'react'
+import Header from './Header'
+import MemeGenerator from './MemeGenerator'
 import './style.css'
 
 export default function App() {
     return (
         <div>
-            this is an app
+            <Header />
+            <MemeGenerator />
         </div>
     )
 }
---
+++ Module-3/meme-generator/src/MemeGenerator.js
@@ -0,0 +1,20 @@
+import React, {Component} from 'react'
+
+export default class MemeGenerator extends Component {
+    constructor() {
+        super()
+        this.state = {
+            memes: []
+        }
+    }
+
+    componentDidMount () {
+        
+    }
+
+    render() {
+        return (
+            <h1>MEME GENERATOR SECTION</h1>
+        )
+    }
+}
