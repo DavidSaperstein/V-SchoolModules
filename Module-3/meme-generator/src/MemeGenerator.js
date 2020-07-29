532b76e9c5602a9ed5f42f296acdbbe9708d4a2c
DavidSaperstein <d.h.saperstein@gmail.com>
2020-07-29 00:45:06 -0600

    added axios call for all meme imgs

--- Module-3/meme-generator/src/MemeGenerator.js
+++ Module-3/meme-generator/src/MemeGenerator.js
@@ -1,4 +1,5 @@
 import React, {Component} from 'react'
+import axios from 'axios'
 
 export default class MemeGenerator extends Component {
     constructor() {
@@ -6,12 +7,19 @@ export default class MemeGenerator extends Component {
         this.state = {
             topText: '',
             bottomText: '',
-            randomImg: 'http://i.imgflip.com/1bij.jpg'
+            randomImg: 'http://i.imgflip.com/1bij.jpg',
+            allMemeImgs: []
         }
     }
 
     componentDidMount () {
-        
+        axios.get('https://api.imgflip.com/get_memes')
+            .then(response => {
+                const {memes} = response.data.data
+                console.log(memes[0])
+                this.setState({allMemeImgs: memes})
+            })
+            .catch(err => console.log(err))
     }
 
     render() {
