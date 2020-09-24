c99b4be025f441a4caff60f26cae42a43c4c62b2
DavidSaperstein <d.h.saperstein@gmail.com>
2020-09-24 14:07:40 -0600

    form complete maybe

--- Module-4/ugly-things/src/App.js
+++ Module-4/ugly-things/src/App.js
@@ -1,14 +1,15 @@
 import React, {useContext} from 'react'
 import './styles.css'
-import {ImageList} from './context'
+import {FormContext} from './context'
 import Form from './Form'
 import ImageCard from './ImageCard'
 
 function App (props) {
+    const {images} = useContext(FormContext)
     return (
         <div>
             <Form />
-            {ImageList.map(image, i => <ImageCard key={i} title={image.title} image={image.url} comment={image.comment}/>)}
+            {images.map(image, i => <ImageCard key={i} title={image.title} image={image.url} description={image.description}/>)}
         </div>
     )
 }
--- Module-4/ugly-things/src/Form.js
+++ Module-4/ugly-things/src/Form.js
@@ -1,3 +1,14 @@
 import React, {useContext} from 'react'
-import {formContext} from './context'
+import {FormContext} from './context'
 
+const Form = (props) => {
+    const {title, url, description, submit} = useContext(FormContext)
+    return (
+        <form onClick={submit}>
+            <input name='title' value={title} onChange={(e) => setTitle(e.target.value)}/>
+            <input name='url' value={url} onChange={(e) => setUrl(e.target.value)}/>
+            <input name='description' value={description} onChange={(e) => setDescription(e.target.value)}/>
+            <button>Submit</button>
+        </form>
+    )
+}
