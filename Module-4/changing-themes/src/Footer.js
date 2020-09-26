cc9eb105d1291fe00ae763195e3b95e3e318bd3e
DavidSaperstein <d.h.saperstein@gmail.com>
2020-09-26 16:03:20 -0600

    merge

--- Module-4/changing-themes/src/Footer.js
+++ Module-4/changing-themes/src/Footer.js
@@ -2,7 +2,6 @@ import React, {useContext} from 'react'
 import {ThemeContext} from './themeContext'
 
 function Footer(props) {
-    /**/
     const {theme} = useContext(ThemeContext)
     return (
         <div className={`${theme}-footer`}>
