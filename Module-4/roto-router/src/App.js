84fe7cfa370078be2ba5e46685fb095257e27e06
DavidSaperstein <d.h.saperstein@gmail.com>
2020-10-12 16:40:59 -0600

    Switch in App complete

--- Module-4/roto-router/src/App.js
+++ Module-4/roto-router/src/App.js
@@ -1,4 +1,5 @@
 import react from 'react'
+import {Switch, Route} from 'react-router-dom'
 import Header from './Header'
 import Main from './Main'
 import Footer from './Footer'
@@ -8,7 +9,21 @@ const App = (props) => {
   return (
       <>
         <Header />
-        <Main />
+
+        <Switch>
+          <Route exact path='/'>
+            <Home />
+          </Route>
+
+          <Route exact path='/about'>
+            <About />
+          </Route>
+
+          <Route exact path='/services'>
+            <Services />
+          </Route>
+        </Switch>
+
         <Footer />
       </>
   )
---
+++ Module-4/roto-router/src/Navbar.js
@@ -0,0 +1,6 @@
+import react from 'react'
+import {Link} from 'react-router-dom'
+
+const Navbar = (props) => {
+
+}
