c2494a65d86197a70e288673e3ef0d62701a38af
DavidSaperstein <d.h.saperstein@gmail.com>
2020-10-12 16:44:02 -0600

    Links added to Navbar

--- Module-4/roto-router/src/Navbar.js
+++ Module-4/roto-router/src/Navbar.js
@@ -3,4 +3,17 @@ import {Link} from 'react-router-dom'
 
 const Navbar = (props) => {
 
+  return (    
+    <div>
+      <Link to='/'>
+        Home
+      </Link>
+      <Link to='/about'>
+        About
+      </Link>
+      <Link to='/services'>
+        Services
+      </Link>
+    </div>
+  )
 }
